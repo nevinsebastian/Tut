8cd10628e855ad492e52514e52cb8feb822bd059
nevinsebastian <nevinsebastian@cse.carmelcet.in>
2024-01-09 23:00:54 +0530

    flat screens

--- App.tsx
+++ App.tsx
@@ -1,28 +1,16 @@
-import React from "react";
+import { View, Text, SafeAreaView, ScrollView } from 'react-native'
+import React from 'react'
+import FlatCards from './components/FlatCards'
 
-import { View,
-         Text,
-         SafeAreaView 
-      } from "react-native";
-
-
-
-function App(){
-return(
-  <SafeAreaView>
-    <View>
-      <Text>Hello World!!</Text>
-      <Text>Hello World!!</Text>
-      <Text>Hello World!!</Text>
-      <Text>Hello World!!</Text>
-      <Text>Hello World!!</Text>
-      <Text>Hello World!!</Text>
-
-    </View>
-  </SafeAreaView>
-)
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+      <Text>App</Text>
+      <FlatCards/>
+      </ScrollView>
+    </SafeAreaView>
+  )
 }
 
-
-
-export default App;
+export default App
